84d14ededca2a5ef945a3bfc73e3703012d1b11e
Juan Pablo Caballero <juan.ccastellanos@mail.escuelaing.edu.co>
2026-03-19 21:39:19 -0500

    preprod: pre-producción de registro, login y perfil

--- figth-club/src/modules/authentication/config/axios.ts
+++ figth-club/src/modules/authentication/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const authApi = axios.create({
-  baseURL: 'https://clubfigth-e9ffa5bjeeb4bvb7.brazilsouth-01.azurewebsites.net',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: { 'Content-Type': 'application/json' },
 });
 
