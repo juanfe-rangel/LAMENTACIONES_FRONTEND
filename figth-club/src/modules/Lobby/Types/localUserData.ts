93e95a283f22e174e951de3f2008587580ff5373
SantiagoSu15 <sasupu08@gmail.com>
2026-04-01 11:33:25 -0500

    test cambio de user id

--- figth-club/src/modules/Lobby/Types/localUserData.ts
+++ figth-club/src/modules/Lobby/Types/localUserData.ts
@@ -1,16 +1,11 @@
 export type UserData = {
-    userId: string;
-    username: string;
-    email: string;
-    role: string;
-};
-  
-export const getUserData = (): UserData | null => {
-    const raw = localStorage.getItem('user_data');
-    return raw ? JSON.parse(raw) : null;
+  userId: string;
+  username: string;
+  email: string;
+  role: string;
 };
 
-export const userDataRaw = localStorage.getItem('user_data');
-export  const userDataLocalStorage: UserData | null = userDataRaw 
-      ? JSON.parse(userDataRaw) 
-      : null;
+export const getUserData = (): UserData | null => {
+  const raw = localStorage.getItem("user_data");
+  return raw ? JSON.parse(raw) : null;
+};
